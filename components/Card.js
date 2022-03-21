6c9b0cf3e66e8a09e05dc8298f3caef2a7685149
trinhcuong297 <trinhcuong297@gmail.com>
2022-03-21 18:47:24 +0700

    card-fix

--- components/Card.js
+++ components/Card.js
@@ -1,4 +1,3 @@
-import Image from './Image'
 import Link from './Link'
 
 const Card = ({ title, description, imgSrc, href, height }) => (
@@ -8,8 +7,8 @@ const Card = ({ title, description, imgSrc, href, height }) => (
         imgSrc && 'h-full'
       }  overflow-hidden rounded-md border border-indigo-600 border-opacity-60 shadow-md shadow-indigo-500/50 dark:border-gray-700`}
     >
-      <div className="flex h-full flex-col justify-between p-4">
-        <div className="">
+      <div className="flex h-full flex-col justify-between pt-4 px-4">
+        <div className="max-h-32 md:max-h-max">
           <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight text-blue-500">
             {href ? (
               <Link href={href} aria-label={`Link to ${title}`}>
@@ -21,15 +20,13 @@ const Card = ({ title, description, imgSrc, href, height }) => (
           </h2>
           <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
         </div>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Học ngay &rarr;
-          </Link>
-        )}
+        <Link
+          href={href}
+          className="pb-4 bg-white dark:bg-gray-900 text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+          aria-label={`Link to ${title}`}
+        >
+          Học ngay &rarr;
+        </Link>
       </div>
     </div>
   </div>
