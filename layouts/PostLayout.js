9ba36c0c78f4da6221893b18ce9b25a504e24075
trinhcuong297 <trinhcuong297@gmail.com>
2022-04-02 20:57:51 +0700

    fixPostLayout2/4

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -85,7 +85,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                 </ul>
               </dd>
             </dl>
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:px-6 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(slug)} rel="nofollow">
