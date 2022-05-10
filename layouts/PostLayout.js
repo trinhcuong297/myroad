6f183758878ff0c4393569d589da319c76ef1ba5
trinhcuong297 <trinhcuong297@gmail.com>
2022-05-10 22:18:24 +0700

    fixBugPostLayout-10/5

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -30,8 +30,8 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
       />
       <ScrollTopAndComment />
       <article>
-        <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-          <header className="pt-6 xl:pb-6">
+        <div className="lg:divide-y lg:divide-gray-200 lg:dark:divide-gray-700">
+          <header className="pt-6 lg:pb-6">
             <div className="space-y-1 text-center">
               <dl className="space-y-10">
                 <div>
@@ -49,13 +49,13 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             </div>
           </header>
           <div
-            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0"
+            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 lg:grid lg:grid-cols-4 lg:gap-x-6 lg:divide-y-0"
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+            <dl className="pt-6 pb-10 lg:border-b lg:border-gray-200 lg:pt-11 lg:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
               <dd>
-                <ul className="flex justify-center space-x-8 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+                <ul className="flex justify-center space-x-8 sm:space-x-12 lg:block lg:space-x-0 lg:space-y-8">
                   {authorDetails.map((author) => (
                     <li className="flex items-center space-x-2" key={author.name}>
                       {author.avatar && (
@@ -87,7 +87,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                 </ul>
               </dd>
             </dl>
-            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:px-6 xl:pb-0">
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 lg:col-span-3 lg:row-span-2 lg:px-6 lg:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
                 <Link href={discussUrl(slug)} rel="nofollow">
@@ -107,11 +107,11 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                 boxShadow:
                   'rgb(0 146 255 / 30%) 0px 1px 2px 0px, rgb(94 135 159 / 30%) 0px 2px 6px 2px',
               }}
-              className="mb-4 rounded-md border-[1px] border-indigo-600 border-opacity-60 p-4 shadow-md shadow-indigo-500/50 dark:border-gray-700 xl:fixed xl:bottom-0 xl:w-[18.5rem]"
+              className="mb-4 rounded-md border-[1px] border-indigo-600 border-opacity-60 p-4 shadow-md shadow-indigo-500/50 dark:border-gray-700 lg:fixed lg:bottom-0 lg:w-[18.5rem]"
             >
-              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 lg:col-start-1 lg:row-start-2 lg:divide-y">
                 {tags && (
-                  <div className="py-1 xl:pb-4">
+                  <div className="py-1 lg:pb-4">
                     <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
                       Chủ đề
                     </h2>
@@ -123,7 +123,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   </div>
                 )}
                 {(next || prev) && (
-                  <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-4">
+                  <div className="flex justify-between py-4 lg:block lg:space-y-8 lg:py-4">
                     {prev && (
                       <div>
                         <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
@@ -147,7 +147,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   </div>
                 )}
               </div>
-              <div className="pt-4 pb-2 xl:pt-4">
+              <div className="pt-4 pb-2 lg:pt-4">
                 <Link
                   href="/blog"
                   className="text-indigo-500 hover:text-indigo-600 dark:hover:text-indigo-400"
