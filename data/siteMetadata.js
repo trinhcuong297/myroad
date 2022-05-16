a1800bdcd2a24854daf1331feb9841eba8f90978
trinhcuong297 <trinhcuong297@gmail.com>
2022-05-16 19:53:52 +0700

    changeMdfileAndAddRobots.txt

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,7 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: '', // e.g. myroad.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
